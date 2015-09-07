09352945f6c598cfbc285694005ddbb0afc02607
salismt <tsalis.mohammad@gmail.com>
2015-09-07 19:10:24 +0700

    initial working code

---
+++ public/modules/categories/config/categories.client.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Categories module config
+angular.module('categories').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Categories', 'categories', 'dropdown',
+			'/categories(/create)?');
+		Menus.addSubMenuItem('topbar', 'categories', 'List Categories',
+			'categories');
+		Menus.addSubMenuItem('topbar', 'categories', 'New Category', 
+			'categories/create');
+	}
+]);
---
+++ public/modules/categories/services/categories.client.service.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Categories service used to communicate Categories REST endpoints
+angular.module('categories').factory('Categories', ['$resource',
+	function($resource) {
+		return $resource('categories/:categoryId', { 
+			categoryId: '@_id'
+		}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+]);
